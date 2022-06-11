ac561c4b6ea13f7ec2814cea9614970ab78d9307
vkrpk <victor.krupka@orange.fr>
2022-06-11 12:04:09 +0200

    change params dbconnection into route.php

--- routes/Route.php
+++ routes/Route.php
@@ -32,7 +32,7 @@ class Route
     public function execute()
     {
         $params = explode('@', $this->action);
-        $controller = new $params[0](new DBConnection('norbus', '127.0.0.1', 'root', ''));
+        $controller = new $params[0](new DBConnection("heroku_a07462fa3a91fd4", 'eu-cdbr-west-02.cleardb.net', 'b857ac46ef3acc', '83d0638c'));
         $method = $params[1];
 
         return isset($this->matches[1]) ? $controller->$method($this->matches[1]) : $controller->$method();
